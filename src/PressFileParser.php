abc9980d2e13684b010db78e2932c02717535c6e
zendzo <zendzzo@gmail.com>
2020-11-10 15:49:35 +0700

    split rawData from data

--- src/PressFileParser.php
+++ src/PressFileParser.php
@@ -4,52 +4,66 @@ namespace zendzo\Press;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class PressFileParser
 {
-  protected $filename;
+	protected $filename;
 
-  protected $data;
+	protected $data;
 
-  public function __construct($filename)
-  {
-    $this->filename = $filename;
-    $this->splitFile();
-    $this->explodeData();
-    $this->processFileds();
-  }
+	protected $rawData;
 
-  public function getData()
-  {
-    return $this->data;
-  }
+	public function __construct($filename)
+	{
+		$this->filename = $filename;
+		$this->splitFile();
+		$this->explodeData();
+		$this->processFileds();
+	}
 
-  protected function splitFile()
-  {
-    preg_match('/^\-{3}(.*?)\-{3}(.*)/s', 
-    File::exists($this->filename) ? File::get($this->filename) : $this->filename, 
-    $this->data
-    );
-  }
+	public function getData()
+	{
+		return $this->data;
+	}
 
-  protected function explodeData()
-  {
-      foreach (explode("\n", trim($this->data[1])) as $fieldString) {
-        preg_match('/(.*):\s?(.*)/', $fieldString, $fieldArray);
-        $this->data[$fieldArray[1]] = $fieldArray[2];
-      }
+	public function getRawData()
+	{
+		return $this->rawData;
+	}
 
-      $this->data['body'] = trim($this->data[2]);
-  }
+	protected function splitFile()
+	{
+		preg_match(
+			'/^\-{3}(.*?)\-{3}(.*)/s',
+			File::exists($this->filename) ? File::get($this->filename) : $this->filename,
+			$this->rawData
+		);
+	}
 
-  public function processFileds()
-  {
-    foreach ($this->data as $filed => $value) {
-      if ($filed === 'date') {
-        $this->data[$filed] = Carbon::parse($value);
-      }elseif ($filed === 'body') {
-        $this->data['body'] = MarkdownParser::parse($value);
-      }
-    }
-  }
+	protected function explodeData()
+	{
+		foreach (explode("\n", trim($this->rawData[1])) as $fieldString) {
+			preg_match('/(.*):\s?(.*)/', $fieldString, $fieldArray);
+			$this->data[$fieldArray[1]] = $fieldArray[2];
+		}
+
+		$this->data['body'] = trim($this->rawData[2]);
+	}
+
+	public function processFileds()
+	{
+		foreach ($this->data as $field => $value) {
+
+			$class = 'zendzo\\Press\\Fields\\' . Str::title($field);
+
+			if ( !class_exists($class) && !method_exists($class, 'process')) {
+				$class = 'zendzo\\Press\\Fields\\Extra';
+			}
+			$this->data = array_merge(
+				$this->data,
+				$class::process($field, $value, $this->data),
+			);
+		}
+	}
 }
