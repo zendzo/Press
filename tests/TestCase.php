71be7653a593b83405ad6033daf90eba1394fade
zendzo <zendzzo@gmail.com>
2020-11-11 11:13:32 +0700

    getEnvironmentSetup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,12 +7,21 @@ use zendzo\Press\PressBaseServiceProvider;
 
 // the base class TestCase should be abstract class https://github.com/sebastianbergmann/phpunit/issues/3916#issuecomment-547584947
 // otherwise PHPUnit  return a warning no test in TestCase
-abstract class TestCase extends TestbenchTestCase
+abstract class TestCase extends TestCase
 {
   public function getPackageProviders($app)
   {
     return [
       PressBaseServiceProvider::class
     ];
+  }
+
+  public function getEnvironmentSetup($app)
+  {
+      $app['config']->set('database.default', 'testdb');
+      $app['config']->set('database.connections.testdb ', [
+        'driver' => 'sqlite',
+        'database' => ':memory:'
+      ]);
   }
 }
